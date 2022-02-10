c0d2751007bf2ba174a10f1892da25ebc7bae5b9
OmoladeAkingbade <lademercy93@gmail.com>
2022-02-10 17:21:07 +0100

    scripts

--- src/pgconfig.ts
+++ src/pgconfig.ts
@@ -1,26 +1,24 @@
-import { ConnectionOptions } from "typeorm"
-import { User } from "./entities/userEntity"
-import { Twit } from "./entities/twitEntity"
-import { Comment } from "./entities/commentEntity"
-import { Like } from "./entities/likeEntity"
-import dotenv from "dotenv";
+import { ConnectionOptions } from 'typeorm';
+import { User } from './entities/userEntity';
+import { Twit } from './entities/twitEntity';
+import { Comment } from './entities/commentEntity';
+import { Like } from './entities/likeEntity';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-
- const pgconfig: ConnectionOptions = {
-  type: "postgres",
+const pgconfig: ConnectionOptions = {
+  type: 'postgres',
   host: process.env.PG_HOST,
   port: +(process.env.PG_PORT as string),
   username: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DB,
   entities: [User, Twit, Comment, Like],
-  migrations: ["migration/*.js"],
-  cli: { migrationsDir: "migration" },
+  migrations: ['migration/*.js'],
+  cli: { migrationsDir: 'migration' },
+  ssl: { rejectUnauthorized: false },
   // syncronize: true,
-}
+};
 
-
-
-export = pgconfig
+export = pgconfig;
